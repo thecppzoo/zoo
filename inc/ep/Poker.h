7713cce7a965b70c04c11a60445eee0b4060ec93
Eduardo <emadrid@crabel.com>
2017-02-23 02:18:40 -0500

    Refactoring

--- inc/ep/Poker.h
+++ inc/ep/Poker.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "PokerTypes.h"
-#include "ep/metaBinomial.h"
 #include "ep/core/metaLog.h"
 
 namespace ep {
@@ -36,9 +35,7 @@ struct Counted {
         return bestBit(m_counts.value()) / Size;
     }
 
-    constexpr Counted clearAt(int index) {
-        return m_counts.clear(index);
-    }
+    constexpr Counted clearAt(int index) { return m_counts.clear(index); }
 
     protected:
     core::SWAR<Size, T> m_counts;
@@ -47,44 +44,6 @@ struct Counted {
 template<int Size, typename T>
 constexpr Counted<Size, T>
 makeCounted(core::SWAR<Size, T> bits) { return bits; }
-
-struct MonotoneFlop {
-    constexpr static auto equivalents = NSuits;
-    constexpr static auto element_count = Choose<NRanks, 3>::value;
-    constexpr static auto max_repetitions = 0;
-};
-
-/// \todo There are several subcategories here:
-/// The card with the non-repeated suit is highest, pairs high, middle, pairs low, lowest
-struct TwoToneFlop {
-    constexpr static auto equivalents = PartialPermutations<NSuits, 2>::value;
-    constexpr static auto element_count =
-        NRanks * Choose<NRanks, 2>::value;
-    constexpr static auto max_repetitions = 1;
-};
-
-struct RainbowFlop {
-    constexpr static auto equivalents = Choose<NSuits, 3>::value;
-    constexpr static auto element_count = NRanks*NRanks*NRanks;
-    constexpr static auto max_repetitions = 2;
-};
-
-template<typename...> struct Count {
-    constexpr static uint64_t value = 0;
-};
-template<typename H, typename... Tail>
-struct Count<H, Tail...> {
-    constexpr static uint64_t value =
-        H::equivalents*H::element_count + Count<Tail...>::value;
-};
-
-static_assert(4 * Choose<13, 3>::value == Count<MonotoneFlop>::value, "");
-static_assert(12*13*Choose<NRanks, 2>::value == Count<TwoToneFlop>::value, "");
-static_assert(4*13*13*13 == Count<RainbowFlop>::value, "");
-static_assert(
-    Choose<52, 3>::value == Count<MonotoneFlop, TwoToneFlop, RainbowFlop>::value,
-    ""
-);
 
 struct CSet {
     SWARSuit m_bySuit;
