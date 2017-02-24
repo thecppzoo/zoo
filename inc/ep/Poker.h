665e30b4f9b8f966da8be815f8ca885fe141e18f
Eduardo <emadrid@crabel.com>
2017-02-23 21:09:05 -0500

    Corrected known bugs

--- inc/ep/Poker.h
+++ inc/ep/Poker.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include "PokerTypes.h"
-#include "ep/core/metaLog.h"
 
 namespace ep {
 
 constexpr int64_t positiveIndex1Better(uint64_t index1, uint64_t index2) {
-    return index2 - index1;
+    return index1 - index2;
 }
 
 template<int Size, typename T>
@@ -130,49 +129,49 @@ union HandRank {
     uint32_t code;
     struct {
         unsigned
-            hand: 4,
+            low: 14,
             high: 14,
-            low: 14;
+            hand: 4;
     };
-};
 
-inline uint32_t handRankCode(BestHand h, uint16_t high, uint16_t low) {
-    HandRank rv;
-    rv.hand = h;
-    rv.high = high;
-    rv.low = low;
-    return rv.code;
-}
+    HandRank(): code(0) {}
+    HandRank(BestHand h, int high, int low):
+        hand(h), high(high), low(low)
+    {}
+
+    constexpr operator bool() { return code; }
+};
 
 inline unsigned reverseBits(unsigned input) { return input; }
 
-uint32_t handRank(CSet hand) {
+HandRank handRank(CSet hand) {
     auto rankCounts = hand.rankCounts();
     auto suitCounts = hand.suitCounts();
     auto ranks = hand.rankSet();
     auto toaks = noaks<3>(rankCounts);
-    uint32_t rv = 0;
+    HandRank rv;
     RARE(toaks) {
         auto foaks = noaks<4>(rankCounts);
         RARE(foaks) {
             static_assert(TotalHand < 8, "There can be four-of-a-kind and straight-flush");
             auto foak = foaks.best();
             auto without = rankCounts.clearAt(foak);
-            return handRankCode(FOUR_OF_A_KIND, without.best(), 0);
+            return { FOUR_OF_A_KIND, 1 << foak, 1 << without.best() };
         }
         auto toak = toaks.best();
         auto without = rankCounts.clearAt(toak);
         auto pairs = noaks<2>(without);
         RARE(pairs) {
             static_assert(TotalHand < 8, "There can be full-house and straight-flush");
-            return handRankCode(FULL_HOUSE, toak, pairs.best());
+            return { FULL_HOUSE, (1 << toak) | (1 << pairs.best()), 0 };
         }
-        ranks ^= (1 << toak);
+        auto high = 1 << toak;
+        ranks ^= high;
         auto next = core::msb(ranks);
-        auto low = 1 << next;
-        ranks ^= low;
-        low |= 1 << core::msb(ranks);
-        rv = handRankCode(THREE_OF_A_KIND, toak, low);
+        auto lowTwo = 1 << next;
+        ranks ^= lowTwo;
+        lowTwo |= 1 << core::msb(ranks);
+        rv = HandRank(THREE_OF_A_KIND, high, lowTwo);
     }
     auto flushes = suitCounts.greaterEqual<5>();
     RARE(flushes) {
@@ -181,17 +180,15 @@ uint32_t handRank(CSet hand) {
         auto royal = hand.m_bySuit.at(suit);
         auto isIt = straights(royal);
         RARE(isIt) {
-            return handRankCode(STRAIGHT_FLUSH, core::msb(isIt), 0);
+            return { STRAIGHT_FLUSH, 1 << core::msb(isIt), 0 };
         }
         for(auto count = suitCounts.counts().at(suit) - 5; count--; ) {
             royal &= (royal - 1); // turns off lsb
         }
-        return handRankCode(FLUSH, royal, 0);
+        return { FLUSH, int(royal), 0 };
     }
     auto str = straights(ranks);
-    RARE(str) {
-        return handRankCode(STRAIGHT, core::msb(str), 0);
-    }
+    RARE(str) { return { STRAIGHT, 1 << core::msb(str), 0 }; }
     RARE(rv) { return rv; }
     auto pairs = rankCounts.greaterEqual<2>();
     if(pairs) {
@@ -201,7 +198,7 @@ uint32_t handRank(CSet hand) {
             auto bottom = without.best();
             auto high = (1 << top) | (1 << bottom);
             ranks ^= high;
-            return handRankCode(TWO_PAIRS, high, core::msb(ranks));
+            return { TWO_PAIRS, high, 1 << core::msb(ranks) };
         }
         auto high = 1 << top;
         ranks ^= high;
@@ -209,34 +206,13 @@ uint32_t handRank(CSet hand) {
         ranks ^= next1;
         auto next2 = 1 << core::msb(ranks);
         ranks ^= next2;
-        return handRankCode(PAIR, high, next1 | next2 | core::msb(ranks));
+        auto next3 = 1 << core::msb(ranks);
+        return { PAIR, high, next1 | next2 | next3 };
     }
     for(auto count = TotalHand - 5; count--; ) {
         ranks &= ranks - 1;
     }
-    return handRankCode(HIGH_CARDS, ranks, 0);
+    return { HIGH_CARDS, int(ranks), 0 };
 }
 
 }
-
-#if 0
-A  B  C  D  E  F  G  H  I  J  K  L  M  N  r0
------------------------------------------
-B  C  D  E  F  G  H  I  J  K  L  M  N  -  r0s1 = r0 << 1 1, 0
------------------------------------------
-A  B  C  D  E  F  G  H  I  J  K  L  M  N 
-B  C  D  E  F  G  H  I  J  K  L  M  N  -  r1 = r0 & r0s1 1, 1
------------------------------------------        
-C  D  E  F  G  H  I  J  K  L  M  N  -  -
-D  E  F  G  H  I  J  K  L  M  N  -  -  -  r1s2 = r1 << 2 2, 1
------------------------------------------
-A  B  C  D  E  F  G  H  I  J  K  L  M  N
-B  C  D  E  F  G  H  I  J  K  L  M  N  -
-C  D  E  F  G  H  I  J  K  L  M  N  -  -
-D  E  F  G  H  I  J  K  L  M  N  -  -  -  r2 = r1 & r1s2 2, 2
------------------------------------------
-E  F  G  H  I  J  K  L  M  N  -  -  -  -  r0s4 = r0 << 4 3, 2
------------------------------------------
-        result                            r2 & r0s4      3, 3
- 
-#endif
