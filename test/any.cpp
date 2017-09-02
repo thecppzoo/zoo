55443cea6b71c7fd47b9ac08fdaea8a0a0c851f8
Eddie <nothing@not.com>
2017-09-02 13:58:49 -0700

    Systematic unit tests

--- test/any.cpp
+++ test/any.cpp
@@ -49,6 +49,53 @@ void moveConstructorNoexcept(zoo::Any a) {
 #endif
 #include "catch.hpp"
 
+TEST_CASE("IAnyContainer") {
+    using namespace zoo;
+    using Container = IAnyContainer<sizeof(void *), alignof(void *)>;
+
+    alignas(Container) char bytes[sizeof(Container)];
+    new(bytes) Container;
+    auto &container = *reinterpret_cast<Container *>(bytes);
+    auto setTo0x33 = [](auto ptr, auto count) {
+        for(auto i = count; i--; ) {
+            ptr[i] = 0x33;
+        }
+    };
+    auto check0x33 = [](auto ptr, auto count) {
+        for(auto i = count; i--; ) {
+            if(0x33 != ptr[i]) { return false; }
+        }
+        return true;
+    };
+    setTo0x33(container.m_space, sizeof(container.m_space));
+    SECTION("Performance issue: constructor changes bytes") {
+        new(bytes) Container;
+        CHECK(check0x33(container.m_space, sizeof(container.m_space)));
+    }
+    Container c;
+    SECTION("Copy does not initialize destination") {
+        c.copy(&container);
+        // the binary representation of a valid IAnyContainer can't be all bytes
+        // set to 0x33
+        CHECK(!check0x33(bytes, sizeof(bytes)));
+    }
+    static_assert(noexcept(c.move(&container)), "move is noexcept(true)");
+    SECTION("Move does not initialize destination") {
+        c.move(&container);
+        CHECK(!check0x33(bytes, sizeof(bytes)));
+    }
+    static_assert(noexcept(c.value()), "value may throw");
+    const auto containerPtr = &container;
+    static_assert(noexcept(containerPtr->nonEmpty()), "nonEmpty may throw");
+    SECTION("nonEmpty is true") {
+        CHECK(!container.nonEmpty());
+    }
+    static_assert(noexcept(containerPtr->type()), "type may throw");
+    SECTION("Type info is not void") {
+        CHECK(typeid(void) == containerPtr->type());
+    }
+}
+
 struct Destructor {
     int *ptr;
 
