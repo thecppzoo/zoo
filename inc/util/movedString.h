826fc6be90651e005167da65beb058f48d53283f
Eddie <nothing@not.com>
2017-09-04 20:53:20 -0700

    Attempt to detect string moves

---
+++ inc/util/movedString.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef NO_STANDARD_INCLUDES
+#include <string>
+#endif
+
+namespace zoo {
+
+using BeforeMovingType = const char *;
+
+BeforeMovingType beforeMoving(const std::string &s) {
+	return s.data();
+}
+
+auto wasMoved(const std::string &to, BeforeMovingType b) {
+	return to.data() == b;
+}
+
+}
