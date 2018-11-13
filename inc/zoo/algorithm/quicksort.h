fe1c3582746c61f921042e9691cf3af727b41b6a
Eddie <nothing@not.com>
2018-11-12 16:24:03 -0800

    Elimination of nesting level

--- inc/zoo/algorithm/quicksort.h
+++ inc/zoo/algorithm/quicksort.h
@@ -14,29 +14,28 @@ namespace zoo {
 template<typename FI, typename Comparison>
 FI implicitPivotPartition(FI b, FI e, Comparison cmp) {
     auto pivot = b++;
-    while(b != e) {
+    for(; b != e; ++b) {
         // invariant: ..., L0, P == *pivot, G0, G1, ... Gn, *b
         // where Lx means lower-than-pivot and Gx higher-equal-to-pivot
-        if(cmp(*b, *pivot)) {
-            // ..., L1, P == *pivot, G0, G1, ..., Gn, L0 == *b, X0, ...
-            // The pivot is greater than the element:
-            // insert *b into the lower partition:
-            //  1. *b goes into the pivot position
-            //  2. the pivot increases by one
-            //  3. the element at pivot + 1, the new pivot, must be greater
-            // than or equal to any Lx, the value of *pivot satisfies this
-            // property.
-            // These requirements can be satisfied by rotating the elements
-            // at positions (pivot, b, pivot + 1)
-            // ..., L1, L0, L == *pivot, G0, G1, ..., P == *b, X0, ...
-            auto oldPivot = pivot++;
-            if(b == pivot) {
-                moveRotation(*oldPivot, *pivot);
-            } else {
-                moveRotation(*oldPivot, *b, *pivot);
-            }
+        if(!cmp(*b, *pivot)) { continue; }
+
+        // ..., L1, P == *pivot, G0, G1, ..., Gn, L0 == *b, X0, ...
+        // The pivot is greater than the element:
+        // insert *b into the lower partition:
+        //  1. *b goes into the pivot position
+        //  2. the pivot increases by one
+        //  3. the element at pivot + 1, the new pivot, must be greater
+        // than or equal to any Lx, the value of *pivot satisfies this
+        // property.
+        // These requirements can be satisfied by rotating the elements
+        // at positions (pivot, b, pivot + 1)
+        // ..., L1, L0, L == *pivot, G0, G1, ..., P == *b, X0, ...
+        auto oldPivot = pivot++;
+        if(b == pivot) {
+            moveRotation(*oldPivot, *pivot);
+        } else {
+            moveRotation(*oldPivot, *b, *pivot);
         }
-        ++b;
     }
     return pivot;
 }
@@ -54,9 +53,11 @@ void quicksort(I begin, I end, Comparison cmp = Comparison{}) {
     for(;;) { // outer loop for doing a frame
         for(;;) { // to do recursion in the lower partition
             auto pivot = implicitPivotPartition(begin, end, cmp);
-            auto successor = pivot;
-            ++successor;
-            stack[index] = { successor, end };
+            if(pivot == end) { // partition sorted
+                if(!index--) { return; }
+                break;
+            }
+            stack[index] = { next(pivot), end };
             end = pivot;
             if(begin == end) { break; }
             if(FrameCount <= ++index) {
