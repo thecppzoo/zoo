8280cdb2021290360d2a671745b86b1e107c3146
Eduardo Madrid <emadrid@snapchat.com>
2019-07-22 18:44:07 -0700

    Rebinding

--- test/AnyCallSignature.cpp
+++ test/AnyCallSignature.cpp
@@ -12,11 +12,24 @@
 
 int doubler(int a) { return 2*a; }
 
+int multiply(int f1, int f2) { return f1*f2; }
+
 TEST_CASE("AnyCallSignature") {
-    using Canonical = zoo::AnyContainer<zoo::RuntimePolymorphicAnyPolicy<sizeof(void *), alignof(void *)>>;
+    using Canonical =
+        zoo::AnyContainer<
+            zoo::RuntimePolymorphicAnyPolicy<sizeof(void *), alignof(void *)>
+        >;
     using ACS = zoo::AnyCallSignature<Canonical>;
     ACS acs(doubler, std::in_place_type<int(int)>);
-    CHECK(6 == acs.call<int(int)>(3));
-    ACS copy(acs);
-    CHECK(8 == copy.call<int(int)>(4));
+    SECTION("Calling") {
+        CHECK(6 == acs.call<int(int)>(3));
+    }
+    SECTION("Constructing from an existing value") {
+        ACS copy(acs);
+        CHECK(8 == copy.call<int(int)>(4));
+    }
+    SECTION("Rebinding, to a different signature") {
+        acs = ACS(multiply, std::in_place_type<int(int, int)>);
+        CHECK(15 == acs.call<int(int, int)>(3, 5));
+    }
 }
