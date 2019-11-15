25e6d7b7d46dd3b4ee13ac857e24cacaf9ea1c7f
Evgenii Zaiakin <ezaiakin@snapchat.com>
2019-11-14 18:10:31 -0800

    [LOOK-59383] Secure Sources Storage for zoo
    Link to JIRA ticket:
    https://jira.sc-corp.net/browse/LOOK-59383
    
    Why this change is needed:
    For several critical repositories like SnapCV and LensCore we are going to prevent sharing sources via GCS. We added new custom attribute to use git for such a purpose.
    
    What exactly is changing:
    export_sources_git attribut is applied
    
    Risks:
    None.

--- conanfile.py
+++ conanfile.py
@@ -4,8 +4,13 @@ class Zoo(nlo.NloBasicCmakeConanFile):
     name = "zoo"
     generators = ("nlo_cmake_link", "cmake", "json")
     exports_sources = ["CMakeLists.txt", "inc/*"]
-    options = {"production": [False, True]}
-    default_options = {"production": False}
+
+    exports_sources_git = {
+        "branch": "auto",
+        "url": "auto",
+        "commit": "auto",
+        "conanfile_rel_path": "auto"
+    }
 
     def build(self):
         self.simple_cmake_build()
