d37cfef1c6c5933bbd66f976d9132595d2bb327c
Eduardo Madrid <emadrid@snapchat.com>
2019-12-23 14:09:34 -0800

    Refactoring, partial generic policy making

--- test/AnyMovable.cpp
+++ test/AnyMovable.cpp
@@ -7,128 +7,18 @@
 
 namespace zoo {
 
-struct VTable: Destroy::VTableEntry, Move::VTableEntry {};
-
-struct VTableHolder {
-    const VTable *ptr_;
-
-    template<typename Affordance>
-    const typename Affordance::VTableEntry *vTable() const noexcept {
-        return static_cast<const typename Affordance::VTableEntry *>(ptr_);
-    }
-
-    VTableHolder(const VTable *p): ptr_(p) {}
+struct CanonicalVTableAny {
+    AlignedStorageFor<typename CanonicalVTablePolicy::MemoryLayout> space_;
 };
 
-struct PseudoContainer:
-    VTableHolder,
-    Destroy::Mixin<PseudoContainer>,
-    Move::Mixin<PseudoContainer>
-{
-    AlignedStorageFor<void *> space_;
-
-    static void moveVTable(void *to, void *from) noexcept {
-        static_cast<PseudoContainer *>(to)->ptr_ =
-            static_cast<PseudoContainer *>(from)->ptr_;
-    }
-
-    constexpr static inline VTable Default = {
-        Destroy::Default<PseudoContainer>,
-        Move::Default<PseudoContainer>
-    };
-
-    PseudoContainer(): VTableHolder(&Default) {}
-
-    using VTableHolder::VTableHolder;
-};
-
-template<typename V>
-struct ByValue: PseudoContainer {
-    V *value() noexcept { return space_.template as<V>(); }
-
-    static void destructor(void *p) noexcept {
-        static_cast<ByValue *>(p)->value()->~V();
-    }
-
-    static void move(void *to, void *from) noexcept {
-        auto downcast = static_cast<ByValue *>(from);
-        new(to) ByValue(std::move(*downcast->value()));
-    }
-
-    constexpr static inline VTable Operations = {
-        Destroy::Operation<ByValue>,
-        Move::Operation<ByValue>
-    };
-
-    template<typename... Args>
-    ByValue(Args &&...args): PseudoContainer(&Operations) {
-        this->space_.template build<V>(std::forward<Args>(args)...);
-    }
-};
-
-template<typename V>
-struct ByReference: PseudoContainer {
-    V *&pValue() noexcept { return *space_.template as<V *>(); }
-    V *value() noexcept { return pValue(); }
-
-    static void destructor(void *p) noexcept {
-        auto ptr = static_cast<ByReference *>(p)->pValue();
-        if(ptr) { delete ptr; }
-    }
-
-    static void move(void *to, void *from) noexcept {
-        auto downcast = static_cast<ByReference *>(from);
-        new(to) ByReference(downcast);
-        downcast->pValue() = nullptr;
-    }
-
-    constexpr static inline VTable Operations = {
-        Destroy::Operation<ByReference>,
-        Move::Operation<ByReference>
-    };
-
-    ByReference(ByReference *source): PseudoContainer(&Operations) {
-        pValue() = source->pValue();
-    }
-
-    template<typename... Args>
-    ByReference(Args &&...args): PseudoContainer(&Operations) {
-        pValue() = new V(std::forward<Args>(args)...);
-    }
-};
-
-template<int S, int A, typename V>
-struct BuilderDecider {
-    constexpr static auto NoThrowMovable =
-        std::is_nothrow_move_constructible_v<V>;
-    constexpr static auto AlignmentSuitable =
-        A % alignof(V) == 0;
-    constexpr static auto SmallBufferSuitable =
-        sizeof(V) <= S && AlignmentSuitable && NoThrowMovable;
-};
-
-struct Policy {
-    using MemoryLayout = PseudoContainer;
-
-    template<typename V>
-    using Builder =
-        std::conditional_t<
-            BuilderDecider<8, 8, V>::SmallBufferSuitable,
-            ByValue<V>,
-            ByReference<V>
-        >;
-};
-
-struct PseudoAny {
-    AlignedStorageFor<typename Policy::MemoryLayout> space_;
-};
+static_assert(impl::HasCopy<CanonicalVTablePolicy::MemoryLayout>::value);
 
 }
 
 #include "catch2/catch.hpp"
 
 TEST_CASE("Ligtests") {
-    using VTA = zoo::AnyContainer<zoo::Policy>;
+    using VTA = zoo::AnyContainer<zoo::CanonicalVTablePolicy>;
     static_assert(std::is_default_constructible_v<VTA>);
     static_assert(std::is_nothrow_move_constructible_v<VTA>);
 
@@ -169,6 +59,10 @@ TEST_CASE("Ligtests") {
             int *resource_;
 
             TracesMoves(int *p): resource_(p) {}
+
+            TracesMoves(const TracesMoves &) {
+                throw std::runtime_error("unreachable called");
+            }
 
             TracesMoves(TracesMoves &&m) noexcept: resource_(m.resource_) {
                 m.resource_ = nullptr;
--- test/inc/zoo/Any/VTablePolicy.h
+++ test/inc/zoo/Any/VTablePolicy.h
@@ -1,4 +1,5 @@
-#include <new>
+#include "zoo/AlignedStorage.h"
+
 #include <typeinfo>
 
 namespace zoo {
@@ -19,7 +20,7 @@ struct Destroy {
         void destroy() noexcept {
             auto downcast = static_cast<Container *>(this);
             downcast->template vTable<Destroy>()->dp(downcast);
-        };
+        }
     };
 };
 
@@ -37,8 +38,175 @@ struct Move {
         void move(void *to) noexcept {
             auto downcast = static_cast<Container *>(this);
             downcast->template vTable<Move>()->mp(to, downcast);
-        };
+        }
     };
+};
+
+struct Copy {
+    struct VTableEntry { void (*cp)(void *, const void *); };
+
+    template<typename Container>
+    constexpr static inline VTableEntry Default = { Container::copyVTable };
+
+    template<typename Container>
+    constexpr static inline VTableEntry Operation = { Container::copy };
+
+    template<typename Container>
+    struct Mixin {
+        void copy(void *to) const {
+            auto downcast = static_cast<const Container *>(this);
+            downcast->template vTable<Copy>()->cp(to, downcast);
+        }
+    };
+};
+
+struct CanonicalVTable:
+    Destroy::VTableEntry, Move::VTableEntry, Copy::VTableEntry {};
+
+template<typename VirtualTable>
+struct VTableHolder {
+    const VirtualTable *ptr_;
+
+    /// \brief from the vtable returns the entry corresponding to the affordance
+    template<typename Affordance>
+    const typename Affordance::VTableEntry *vTable() const noexcept {
+        return static_cast<const typename Affordance::VTableEntry *>(ptr_);
+    }
+
+    VTableHolder(const VirtualTable *p): ptr_(p) {}
+};
+
+struct CanonicalVTableContainer:
+    VTableHolder<CanonicalVTable>,
+    Destroy::Mixin<CanonicalVTableContainer>,
+    Move::Mixin<CanonicalVTableContainer>,
+    Copy::Mixin<CanonicalVTableContainer>
+{
+    AlignedStorageFor<void *> space_;
+
+    static void moveVTable(void *to, void *from) noexcept {
+        static_cast<CanonicalVTableContainer *>(to)->ptr_ =
+            static_cast<CanonicalVTableContainer *>(from)->ptr_;
+    }
+
+    static void copyVTable(void *to, const void *from) {
+        static_cast<CanonicalVTableContainer *>(to)->ptr_ =
+            static_cast<const CanonicalVTableContainer *>(from)->ptr_;
+    }
+
+    constexpr static inline CanonicalVTable Default = {
+        Destroy::Default<CanonicalVTableContainer>,
+        Move::Default<CanonicalVTableContainer>,
+        Copy::Default<CanonicalVTableContainer>
+    };
+
+    CanonicalVTableContainer(): VTableHolder(&Default) {}
+
+    using VTableHolder::VTableHolder;
+};
+
+template<typename... Affordances>
+struct PolicyBuilder {
+    struct VTable: Affordances::VTableEntry... {};
+    using VTHolder = VTableHolder<VTable>;
+    struct Container: VTHolder, Affordances::template Mixin<Container>... {
+        AlignedStorageFor<void *> space_;
+    };
+};
+
+template<typename V>
+struct ByValue: CanonicalVTableContainer {
+    V *value() noexcept { return space_.template as<V>(); }
+    const V *value() const noexcept {
+        return const_cast<ByValue *>(this)->value();
+    }
+
+    static void destructor(void *p) noexcept {
+        static_cast<ByValue *>(p)->value()->~V();
+    }
+
+    static void move(void *to, void *from) noexcept {
+        auto downcast = static_cast<ByValue *>(from);
+        new(to) ByValue(std::move(*downcast->value()));
+    }
+
+    static void copy(void *to, const void *from) {
+        auto downcast = static_cast<const ByValue *>(from);
+        new(to) ByValue(*downcast->value());
+    }
+
+    constexpr static inline CanonicalVTable Operations = {
+        Destroy::Operation<ByValue>,
+        Move::Operation<ByValue>,
+        Copy::Operation<ByValue>
+    };
+
+    template<typename... Args>
+    ByValue(Args &&...args): CanonicalVTableContainer(&Operations) {
+        this->space_.template build<V>(std::forward<Args>(args)...);
+    }
+};
+
+template<typename V>
+struct ByReference: CanonicalVTableContainer {
+    V *&pValue() noexcept { return *space_.template as<V *>(); }
+    V *value() noexcept { return pValue(); }
+    const V *value() const noexcept {
+        return const_cast<ByReference *>(this)->value();
+    }
+
+    static void destructor(void *p) noexcept {
+        auto ptr = static_cast<ByReference *>(p)->pValue();
+        if(ptr) { delete ptr; }
+    }
+
+    static void move(void *to, void *from) noexcept {
+        auto downcast = static_cast<ByReference *>(from);
+        new(to) ByReference(downcast);
+        downcast->pValue() = nullptr;
+    }
+
+    static void copy(void *to, const void *from) {
+        auto downcast = static_cast<const ByReference *>(from);
+        new(to) ByReference(*downcast->value());
+    }
+
+    constexpr static inline CanonicalVTable Operations = {
+        Destroy::Operation<ByReference>,
+        Move::Operation<ByReference>,
+        Copy::Operation<ByReference>
+    };
+
+    ByReference(ByReference *source): CanonicalVTableContainer(&Operations) {
+        pValue() = source->pValue();
+    }
+
+    template<typename... Args>
+    ByReference(Args &&...args): CanonicalVTableContainer(&Operations) {
+        pValue() = new V(std::forward<Args>(args)...);
+    }
+};
+
+template<int S, int A, typename V>
+struct BuilderDecider {
+    constexpr static auto NoThrowMovable =
+        std::is_nothrow_move_constructible_v<V>;
+    constexpr static auto AlignmentSuitable =
+        A % alignof(V) == 0;
+    constexpr static auto SmallBufferSuitable =
+        sizeof(V) <= S && AlignmentSuitable && NoThrowMovable;
+};
+
+struct CanonicalVTablePolicy {
+    using MemoryLayout = CanonicalVTableContainer;
+
+    template<typename V>
+    using Builder =
+        std::conditional_t<
+            BuilderDecider<8, 8, V>::SmallBufferSuitable,
+            ByValue<V>,
+            ByReference<V>
+        >;
 };
 
 }
