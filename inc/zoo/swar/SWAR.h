b56f8d063a633bfad8ab5aac1d8fbce8af9693e2
Scott Bruceheart <scottbruceheart@gmail.com>
2022-06-01 18:41:07 -0700

    add horizontal equality tests at various levels, add broadcast, add (isolate,clear)x(least significant bit, least significant bits). Remove operator== on SWAR. Add MSB indexes as constexpr for boolean swar. Add lsb_index.  Add greaterequal_msb_off. add basic popcount static_asserts

--- inc/zoo/swar/SWAR.h
+++ inc/zoo/swar/SWAR.h
@@ -8,23 +8,28 @@
 
 namespace zoo { namespace swar {
 
+using u64 = uint64_t;
+using u32 = uint32_t;
+using u16 = uint16_t;
+using u8 = uint8_t;
+
 /// Repeats the given pattern in the whole of the argument
 /// \tparam T the desired integral type
 /// \tparam Progression how big the pattern is
 /// \tparam Remaining how many more times to copy the pattern
 template<typename T, int Progression, int Remaining> struct BitmaskMaker {
-    constexpr static T repeat(T v) {
+    constexpr static T repeat(T v) noexcept {
         return
             BitmaskMaker<T, Progression, Remaining - 1>::repeat(v | (v << Progression));
     }
 };
 template<typename T, int P> struct BitmaskMaker<T, P, 0> {
-    constexpr static T repeat(T v) { return v; }
+    constexpr static T repeat(T v) noexcept { return v; }
 };
 
 /// Front end to \c BitmaskMaker with the repeating count set to the whole size
 template<int size, typename T>
-constexpr T makeBitmask(T v) {
+constexpr T makeBitmask(T v) noexcept {
     return BitmaskMaker<T, size, sizeof(T)*8/size>::repeat(v);
 }
 
@@ -36,7 +41,7 @@ namespace detail {
             BitmaskMaker<uint64_t, 1, (1 << level) - 1>::repeat(1)
         );
 
-    static_assert(makeBitmask<2>(1ull) == popcountMask<0>, "");
+    static_assert(makeBitmask<2>(1ull) == popcountMask<0>);
 }
 
 template<int Bits> struct UInteger_impl;
@@ -48,7 +53,7 @@ template<> struct UInteger_impl<64> { using type = uint64_t; };
 template<int Bits> using UInteger = typename UInteger_impl<Bits>::type;
 
 template<int Level>
-constexpr uint64_t popcount_logic(uint64_t arg) {
+constexpr uint64_t popcount_logic(uint64_t arg) noexcept {
     auto v = popcount_logic<Level - 1>(arg);
     constexpr auto shifter = 1 << Level;
     return
@@ -57,7 +62,7 @@ constexpr uint64_t popcount_logic(uint64_t arg) {
 }
 /// Hamming weight of each bit pair
 template<>
-constexpr uint64_t popcount_logic<0>(uint64_t v) {
+constexpr uint64_t popcount_logic<0>(uint64_t v) noexcept {
     // 00: 00; 00
     // 01: 01; 01
     // 10: 01; 01
@@ -66,7 +71,7 @@ constexpr uint64_t popcount_logic<0>(uint64_t v) {
 }
 
 template<int Level>
-constexpr uint64_t popcount_builtin(uint64_t v) {
+constexpr uint64_t popcount_builtin(uint64_t v) noexcept {
     using UI = UInteger<1 << (Level + 3)>;
     constexpr auto times = 8*sizeof(v);
     uint64_t rv = 0;
@@ -83,13 +88,13 @@ namespace detail {
 
     template<bool> struct Selector_impl {
         template<int Level>
-        constexpr static uint64_t execute(uint64_t v) {
+        constexpr static uint64_t execute(uint64_t v) noexcept {
             return popcount_logic<Level>(v);
         }
     };
     template<> struct Selector_impl<true> {
         template<int Level>
-        constexpr static uint64_t execute(uint64_t v) {
+        constexpr static uint64_t execute(uint64_t v) noexcept {
             return popcount_builtin<Level - 2>(v);
         }
     };
@@ -97,85 +102,139 @@ namespace detail {
 }
 
 template<int Level>
-constexpr uint64_t popcount(uint64_t a) {
+constexpr uint64_t popcount(uint64_t a) noexcept {
     return detail::Selector_impl<2 < Level>::template execute<Level>(a);
 }
 
-static_assert(0x210 == popcount<0>(0x320), "");
-static_assert(0x4321 == popcount<1>(0xF754), "");
-static_assert(0x50004 == popcount<3>(0x3E001122), "");
 
-
-template<typename T> constexpr typename std::make_unsigned<T>::type msb_index(T v) {
+template<typename T> constexpr typename std::make_unsigned<T>::type msb_index(T v) noexcept {
     return 8*sizeof(T) - 1 - __builtin_clzll(v);
 }
 
-template<int Size, typename T = uint64_t> struct SWAR {
+template<typename T> constexpr typename std::make_unsigned<T>::type lsb_index(T v) noexcept {
+    return __builtin_ctzll(v) + 1;
+}
+
+template<int NBits, typename T = uint64_t> struct SWAR {
     SWAR() = default;
     constexpr explicit SWAR(T v): m_v(v) {}
+    constexpr explicit operator T() const noexcept { return m_v; }
 
     constexpr T value() const noexcept { return m_v; }
 
-    constexpr SWAR operator|(SWAR o) { return SWAR(m_v | o.m_v); }
-    constexpr SWAR operator&(SWAR o) { return SWAR(m_v & o.m_v); }
-    constexpr SWAR operator^(SWAR o) { return SWAR(m_v ^ o.m_v); }
+    constexpr SWAR operator|(SWAR o) const noexcept { return SWAR(m_v | o.m_v); }
+    constexpr SWAR operator&(SWAR o) const noexcept { return SWAR(m_v & o.m_v); }
+    constexpr SWAR operator^(SWAR o) const noexcept { return SWAR(m_v ^ o.m_v); }
 
-    constexpr bool operator==(SWAR o) { return m_v == o.m_v; }
-
-    constexpr T at(int position) {
-        constexpr auto filter = (T(1) << Size) - 1;
-        return filter & (m_v >> (Size * position));
+    constexpr T at(int position) const noexcept {
+        constexpr auto filter = (T(1) << NBits) - 1;
+        return filter & (m_v >> (NBits * position));
     }
 
-    constexpr SWAR clear(int position) {
-        constexpr auto filter = (T(1) << Size) - 1;
-        auto invertedMask = filter << (Size * position);
+    constexpr SWAR clear(int position) const noexcept {
+        constexpr auto filter = (T(1) << NBits) - 1;
+        auto invertedMask = filter << (NBits * position);
         auto mask = ~invertedMask;
         return SWAR(m_v & mask);
     }
 
-    constexpr int top() { return msb_index(m_v) / Size; }
-    constexpr int fastIndex() { return __builtin_ctzll(m_v) / Size; }
+    /// Notably, this is the SWAR lane index that contains the MSB.
+    constexpr int top() const noexcept { return msb_index(m_v) / NBits; }
+    /// Notably, this is the SWAR lane index that contains the lsb.
+    constexpr int lsbIndex() const noexcept { return __builtin_ctzll(m_v) / NBits; }
 
-    constexpr SWAR set(int index, int bit) {
-        return SWAR(m_v | (T(1) << (index * Size + bit)));
+    constexpr SWAR set(int index, int bit) const noexcept {
+        return SWAR(m_v | (T(1) << (index * NBits + bit)));
     }
 
-    constexpr operator bool() const { return m_v; }
-protected:
     T m_v;
 };
 
-template<int Size, typename T>
-struct BooleanSWAR: SWAR<Size, T> {
-    constexpr BooleanSWAR(T v): SWAR<Size, T>(v) {}
+/// Defining operator== on base SWAR types is entirely too error prone. Force a verbose invocation.
+template<int NBits, typename T = uint64_t> constexpr auto horizontalEquality(SWAR<NBits, T> left, SWAR<NBits, T> right) {
+  return left.m_v == right.m_v;
+}
 
-    constexpr BooleanSWAR clear(int bit) {
-        constexpr auto Bit = T(1) << (Size - 1);
-        return this->m_v ^ (Bit << (Size * bit)); }
-    constexpr int best() { return this->top(); }
+template<int NBits, typename T = uint64_t>
+constexpr auto isolate(T pattern) {
+  return pattern & ((T(1)<<NBits)-1);
+}
 
-    constexpr operator bool() const { return this->m_v; }
+template<typename T = uint64_t>
+constexpr auto clearLSB(T v) {
+  return v & (v - 1);
+}
+
+template<typename T = uint64_t>
+constexpr auto isolateLSB(T v) {
+  return v & ~clearLSB(v);
+}
+
+template<int NBits, typename T = uint64_t>
+constexpr auto maskLowBits() {
+  return (T(1)<<(NBits-1)) | ((T(1)<<(NBits-1))-1);
+}
+
+template<int NBits, typename T = uint64_t>
+constexpr auto clearLSBits(T v) {
+  return v &(~(maskLowBits<NBits>() << metaLogFloor(isolateLSB<T>(v))));
+}
+
+template<int NBits, typename T = uint64_t>
+constexpr auto isolateLSBits(T v) {
+  return v &(maskLowBits<NBits>() << metaLogFloor(isolateLSB<T>(v)));
+}
+
+template<int NBits, typename T = uint64_t>
+constexpr auto broadcast(SWAR<NBits, T> v) {
+  constexpr T Ones = makeBitmask<NBits, T>(SWAR<NBits, T>{1}.value());
+  return SWAR<NBits, T>(T(v) * Ones);
+}
+
+template<int NBits, typename T>
+struct BooleanSWAR: SWAR<NBits, T> {
+    // Booleanness is stored in MSB of a given swar.
+    static constexpr T MSBs = broadcast<NBits, T>(SWAR<NBits, T>(T(1) << (NBits -1))).value();
+    constexpr explicit BooleanSWAR(T v): SWAR<NBits, T>(v) {}
+
+    constexpr BooleanSWAR clear(int bit) const noexcept {
+        constexpr auto Bit = T(1) << (NBits - 1);
+        return this->m_v ^ (Bit << (NBits * bit)); }
+    constexpr int best() const noexcept { return this->top(); }
+
+    constexpr operator bool() const noexcept { return this->m_v; }
+
+    constexpr BooleanSWAR operator not() const noexcept {
+        return MSBs ^ *this;
+    }
+ private:
+    constexpr BooleanSWAR(SWAR<NBits, T> initializer) noexcept: SWAR<NBits, T>(initializer) {}
+
+    template<int NB, typename TT> friend
+    constexpr BooleanSWAR<NB, TT> greaterEqual_MSB_off(SWAR<NB, TT> left, SWAR<NB, TT> right) noexcept;
+
 };
 
-template<int N, int Size, typename T>
-constexpr BooleanSWAR<Size, T> greaterEqualSWAR(SWAR<Size, T> v) {
-    static_assert(1 < Size, "Degenerated SWAR");
-    static_assert(metaLogCeiling(N) < Size, "N is too big for this technique");
-    constexpr auto msbPos  = Size - 1;
+template<int NBits, typename T>
+constexpr BooleanSWAR<NBits, T> greaterEqual_MSB_off(SWAR<NBits, T> left, SWAR<NBits, T> right) noexcept {
+    constexpr auto MSOnes = BooleanSWAR<NBits, T>::MSBs;
+    return SWAR<NBits, T>{
+        (((left.value() | MSOnes) - right.value()) & MSOnes)
+    };
+}
+
+template<int N, int NBits, typename T>
+constexpr BooleanSWAR<NBits, T> greaterEqual(SWAR<NBits, T> v) noexcept {
+    static_assert(1 < NBits, "Degenerated SWAR");
+    //static_assert(metaLogCeiling(N) < NBits, "N is too big for this technique");  // ctzll isn't constexpr.
+    constexpr auto msbPos  = NBits - 1;
     constexpr auto msb = T(1) << msbPos;
-    constexpr auto msbMask = makeBitmask<Size, T>(msb);
-    constexpr auto subtraend = makeBitmask<Size, T>(N);
+    constexpr auto msbMask = makeBitmask<NBits, T>(msb);
+    constexpr auto subtraend = makeBitmask<NBits, T>(N);
     auto adjusted = v.value() | msbMask;
     auto rv = adjusted - subtraend;
     rv &= msbMask;
-    return rv;
+    return BooleanSWAR<NBits, T>(rv);
 }
-
-/// TODO(scottbruceheart) Improve tests
-static_assert(
-    0x80880008 == greaterEqualSWAR<3>(SWAR<4, uint32_t>(0x32451027)).value(),
-    ""
-);
 
 }}
--- test/swar/BasicOperations.cpp
+++ test/swar/BasicOperations.cpp
@@ -6,15 +6,146 @@
 #include <type_traits>
 
 using namespace zoo;
+using namespace zoo::swar;
 
-/// TODO(scottbruceheart) This test is wretched, fix it.
+#define HE(nbits, t, v0, v1) static_assert(horizontalEquality<nbits, t>(SWAR<nbits, t>(v0), SWAR<nbits, t>(makeBitmask<nbits, t>(v1))));
+HE(8, u64, 0x0808'0808'0808'0808, 0x8);
+HE(4, u64, 0x1111'1111'1111'1111, 0x1);
+HE(3, u64, 0xFFFF'FFFF'FFFF'FFFF, 0x7);
+HE(8, u32, 0x0808'0808, 0x8);
+HE(8, u16, 0x0808, 0x8);
+HE(3, u8, 0xFF, 0x7);
+HE(2, u8, 0xAA, 0x2);
+#undef HE
+
 TEST_CASE(
-    "Basic SWAR",
+    "Isolate",
     "[swar]"
 ) {
-    uint8_t v = 0x8;
-    SECTION("Simple Mask") {
-      auto mask = swar::makeBitmask<8, uint32_t>(v);
-      CHECK(mask == 0x0808'0808ull);
-    }
+  for (auto i = 0; i < 63; ++i) { 
+    CHECK(i == isolate<8>(i));
+    CHECK(i == isolate<8>(0xFF00+i));
+    CHECK(i == isolate<8>(0xFFFF00+i));
+  }
+  for (auto i = 0; i < 31; ++i) { 
+    CHECK(i == isolate<7>(i));
+    CHECK(i == isolate<7>(0xFF00+i));
+    CHECK(i == isolate<7>(0xFFFF00+i));
+  }
+  for (auto i = 0; i < 31; ++i) { 
+    CHECK(i == isolate<11>(i));
+    CHECK(i == isolate<11>(0xF800+i));
+    CHECK(i == isolate<11>(0xFFF800+i));
+  }
 }
+
+static_assert(1 == popcount<4>(0x100ull));
+static_assert(1 == popcount<4>(0x010ull));
+static_assert(1 == popcount<4>(0x001ull));
+static_assert(4 == popcount<4>(0xF00ull));
+static_assert(8 == popcount<4>(0xFF0ull));
+static_assert(9 == popcount<4>(0xEEEull));
+static_assert(0x210 == popcount<0>(0x320));
+static_assert(0x4321 == popcount<1>(0xF754));
+static_assert(0x50004 == popcount<3>(0x3E001122));
+
+static_assert(1 == msb_index<u64>(1ull<<1));
+static_assert(3 == msb_index<u64>(1ull<<3));
+static_assert(5 == msb_index<u64>(1ull<<5));
+static_assert(8 == msb_index<u64>(1ull<<8));
+static_assert(17 == msb_index<u64>(1ull<<17));
+static_assert(30 == msb_index<u64>(1ull<<30));
+static_assert(31 == msb_index<u64>(1ull<<31));
+
+static_assert(0xAA == makeBitmask<2, u8>(0x2));
+static_assert(0x0808'0808ull == makeBitmask<8, u32>(0x8));
+static_assert(0x0808'0808'0808'0808ull == makeBitmask<8, u64>(0x08ull));
+static_assert(0x0101'0101'0101'0101ull == makeBitmask<8, u64>(0x01ull));
+static_assert(0x0E0E'0E0E'0E0E'0E0Eull == makeBitmask<8, u64>(0x0Eull));
+static_assert(0x0303'0303'0303'0303ull == makeBitmask<8, u64>(0x03ull));
+
+static_assert(0x00 == clearLSB<u8>(0x80));
+static_assert(0x80 == clearLSB<u8>(0xC0));
+static_assert(0xC0 == clearLSB<u8>(0xE0));
+static_assert(0xE0 == clearLSB<u8>(0xF0));
+static_assert(0xF0 == clearLSB<u8>(0xF8));
+static_assert(0xF8 == clearLSB<u8>(0xFC));
+static_assert(0xFC == clearLSB<u8>(0xFE));
+static_assert(0xFE == clearLSB<u8>(0xFF));
+static_assert(0x00 == clearLSB<u8>(0x00));
+
+static_assert(0x00 == isolateLSB<u8>(0x00));
+static_assert(0x10 == isolateLSB<u8>(0xF0));
+static_assert(0x20 == isolateLSB<u8>(0xE0));
+static_assert(0x40 == isolateLSB<u8>(0xC0));
+static_assert(0x80 == isolateLSB<u8>(0x80));
+
+static_assert(0x01 == maskLowBits<1, u8>());
+static_assert(0x03 == maskLowBits<2, u8>());
+static_assert(0x07 == maskLowBits<3, u8>());
+static_assert(0x0F == maskLowBits<4, u8>());
+
+static_assert(0xB == isolate<4>(0x1337'BDBC'2448'ACABull));
+static_assert(0xAB == isolate<8>(0x1337'BDBC'2448'ACABull));
+static_assert(0xCAB == isolate<12>(0x1337'BDBC'2448'ACABull));
+static_assert(0xACAB == isolate<16>(0x1337'BDBC'2448'ACABull));
+static_assert(0x3 == isolate<3>(0x1337'BDBC'2448'ACABull));
+
+static_assert(0x00 == clearLSBits<2, u8>(0x80));
+static_assert(0x00 == clearLSBits<2, u8>(0xC0));
+static_assert(0x80 == clearLSBits<2, u8>(0xE0));
+static_assert(0xC0 == clearLSBits<2, u8>(0xF0));
+static_assert(0xE0 == clearLSBits<2, u8>(0xF8));
+static_assert(0xF8 == clearLSBits<2, u8>(0xFB));
+static_assert(0xF0 == clearLSBits<2, u8>(0xFC));
+static_assert(0xFC == clearLSBits<2, u8>(0xFF));
+
+static_assert(0x80 == isolateLSBits<2, u8>(0x80));
+static_assert(0xC0 == isolateLSBits<2, u8>(0xC0));
+static_assert(0x60 == isolateLSBits<2, u8>(0xE0));
+static_assert(0x30 == isolateLSBits<2, u8>(0xF0));
+static_assert(0x18 == isolateLSBits<2, u8>(0xF8));
+static_assert(0x03 == isolateLSBits<2, u8>(0xFB));
+static_assert(0x0C == isolateLSBits<2, u8>(0xFC));
+static_assert(0x03 == isolateLSBits<2, u8>(0xFF));
+
+static_assert(0x0606'0606 == u32(broadcast<8>(SWAR<8, u32>(0x0000'0006))));
+static_assert(0x0808'0808 == u32(broadcast<8>(SWAR<8, u32>(0x0000'0008))));
+static_assert(0x0B0B'0B0B == u32(broadcast<8>(SWAR<8, u32>(0x0000'000B))));
+static_assert(0x0E0E'0E0E == u32(broadcast<8>(SWAR<8, u32>(0x0000'000E))));
+static_assert(0x6B6B'6B6B == u32(broadcast<8>(SWAR<8, u32>(0x0000'006B))));
+static_assert(0x0808'0808'0808'0808ull == u64(broadcast<8>(SWAR<8, u64>(0x0000'0000'0000'0008ull))));
+
+static_assert(2 == lsb_index(1<<1));
+static_assert(4 == lsb_index(1<<3));
+static_assert(6 == lsb_index(1<<5));
+static_assert(9 == lsb_index(1<<8));
+static_assert(18 == lsb_index(1<<17));
+static_assert(31 == lsb_index(1<<30));
+
+static_assert(0x80880008 == greaterEqual<3>(SWAR<4, uint32_t>(0x3245'1027)).value());
+static_assert(0x88888888 == greaterEqual<0>(SWAR<4, uint32_t>(0x0123'4567)).value());
+static_assert(0x88888888 == greaterEqual<0>(SWAR<4, uint32_t>(0x7654'3210)).value());
+static_assert(0x00000008 == greaterEqual<7>(SWAR<4, uint32_t>(0x0123'4567)).value());
+static_assert(0x80000000 == greaterEqual<7>(SWAR<4, uint32_t>(0x7654'3210)).value());
+
+static_assert(0x8000'0080 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x1000'0010), SWAR<4, u32>(0x0111'1101)).value());
+static_assert(0x8000'0080 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x4333'3343), SWAR<4, u32>(0x4444'4444)).value());
+
+static_assert(0x8888'8888 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x0123'4567), SWAR<4, u32>(0x0000'0000)).value());
+static_assert(0x0888'8888 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x0123'4567), SWAR<4, u32>(0x1111'1111)).value());
+static_assert(0x0088'8888 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x0123'4567), SWAR<4, u32>(0x2222'2222)).value());
+static_assert(0x0008'8888 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x0123'4567), SWAR<4, u32>(0x3333'3333)).value());
+static_assert(0x0000'8888 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x0123'4567), SWAR<4, u32>(0x4444'4444)).value());
+static_assert(0x0000'0888 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x0123'4567), SWAR<4, u32>(0x5555'5555)).value());
+static_assert(0x0000'0088 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x0123'4567), SWAR<4, u32>(0x6666'6666)).value());
+static_assert(0x0000'0008 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x0123'4567), SWAR<4, u32>(0x7777'7777)).value());
+
+static_assert(0x8000'0000 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x0000'0000), SWAR<4, u32>(0x0123'4567)).value());
+static_assert(0x8800'0000 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x1111'1111), SWAR<4, u32>(0x0123'4567)).value());
+static_assert(0x8880'0000 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x2222'2222), SWAR<4, u32>(0x0123'4567)).value());
+static_assert(0x8888'0000 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x3333'3333), SWAR<4, u32>(0x0123'4567)).value());
+static_assert(0x8888'8000 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x4444'4444), SWAR<4, u32>(0x0123'4567)).value());
+static_assert(0x8888'8800 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x5555'5555), SWAR<4, u32>(0x0123'4567)).value());
+static_assert(0x8888'8880 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x6666'6666), SWAR<4, u32>(0x0123'4567)).value());
+static_assert(0x8888'8888 == greaterEqual_MSB_off<4, u32>(SWAR<4, u32>(0x7777'7777), SWAR<4, u32>(0x0123'4567)).value());
