230159f7fd408e54a4f20c56f1d94223aeafb8fd
Scott Bruceheart <scottbruceheart@gmail.com>
2022-06-04 19:21:29 -0400

    rename msb, leave todo, make bad test more readable with todo'

--- test/swar/BasicOperations.cpp
+++ test/swar/BasicOperations.cpp
@@ -7,6 +7,7 @@
 
 using namespace zoo;
 
+/// TODO(scottbruceheart) This test is wretched, fix it.
 TEST_CASE(
     "Basic SWAR",
     "[swar]"
@@ -14,6 +15,6 @@ TEST_CASE(
     uint8_t v = 0x8;
     SECTION("Simple Mask") {
       auto mask = swar::makeBitmask<8, uint32_t>(v);
-      CHECK(mask == 0x08080808ull);
+      CHECK(mask == 0x0808'0808ull);
     }
 }
