e9b74227beeac863917eeded7eabda49dcf5c566
Eduardo Madrid <emadrid@snapchat.com>
2022-08-13 17:39:04 -0700

    Assemble needle

--- inc/zoo/map/RobinHood.h
+++ inc/zoo/map/RobinHood.h
@@ -14,17 +14,23 @@ struct RH {
         using Base::Base;
     };
 
+    constexpr static inline auto Width = Metadata::NBits;
     Metadata *md_;
 
     // The invariant in Robin Hood is that the element being looked for is "richer"
     // than the present elements.
     // Richer: the PSL of the needle is smaller than the PSL in the haystack
-    static auto potentialMatch(Metadata needle, Metadata *haystack) noexcept {
+    constexpr static auto potentialMatch(Metadata needle, Metadata *haystack) noexcept {
         
     }
 
-    static auto makeNeedle(U startingPsl, U hoistedHash) {
-        //constexpr static auto Ones = meta::BitmaskMaker<U, 1 << , <#int CurrentSize#>>
+    constexpr static auto makeNeedle(U startingPSL, U hoistedHash) {
+        constexpr auto Ones = meta::BitmaskMaker<U, 1, Width>::value;
+        constexpr auto Progression = Ones * Ones;
+        auto core = startingPSL | (hoistedHash << PSL_Bits);
+        auto broadcasted = broadcast(Metadata(core));
+        auto withProgression = Metadata(Progression) + broadcasted;
+        return withProgression;
     }
 };
 
