009200ad89c3db0ad32ce25148cd124bc3d91c7c
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-02-14 12:40:02 -0800

    update platform macros

--- inc/zoo/pp/platform.h
+++ inc/zoo/pp/platform.h
@@ -1,16 +1,16 @@
 #ifndef ZOO_PLATFORM_MACROS_H
 #define ZOO_PLATFORM_MACROS_H
 
-#ifdef _MSC_VER
-
-#define MSVC_EMPTY_BASES __declspec(empty_bases)
-
+#ifdef __AVX2__
+#define ZOO_CONFIGURED_TO_USE_AVX 1
 #else
+#define ZOO_CONFIGURED_TO_USE_AVX 0
+#endif
 
+#ifdef _MSC_VER
+#define MSVC_EMPTY_BASES __declspec(empty_bases)
+#else
 #define MSVC_EMPTY_BASES
-
-#define ZOO_CONFIGURED_TO_USE_AVX defined(__AVX2__) // TODO bring this in from CMake
-
 #endif
 
 #endif
