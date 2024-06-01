d80f0f9b50ed6bc820c43cf8bb03e2e6dcda09a7
Eddie <thecppzoo@gmail.com>
2024-06-01 00:23:15 -0700

    Refactors blockAlignedLoad to the main library

--- benchmark/atoi_impl.h
+++ benchmark/atoi_impl.h
@@ -1,36 +1,24 @@
 #include "zoo/swar/SWAR.h"
 
-#include <tuple>
-#include <string.h>
-
+/// \brief Best implementation of Lemire's technique
+///
+/// 128 integers are currently an emulation that uses 64 computation
+/// together with arquitectural instructions such as
+/// 1. Add with carry
+/// 2. Shifts widened to two general purpose register source/destination operands
+/// 3. Multiplication widened.
+/// In general, 128 bit processing would have significant penalties.
+/// This function reflects our best understanding of how to mix 64 and 128 bit processing
 uint64_t calculateBase10(
+    zoo::swar::SWAR<8, __uint128_t> convertedToIntegers
+) noexcept;
+
+/// \brief Lemire's technique extended to 16 bytes, straightforward use of 128 bit types provided by
+/// the compiler
+uint64_t calculateBase10_128(
     zoo::swar::SWAR<8, __uint128_t> convertedToIntegers
 ) noexcept;
 
 uint32_t calculateBase10(
     zoo::swar::SWAR<8, uint64_t> convertedToIntegers
 ) noexcept;
-
-namespace zoo {
-
-/// @brief Loads the "block" containing the pointer, by proper alignment
-/// @tparam PtrT Pointer type for loading
-/// @tparam Block as the name indicates
-/// @param pointerInsideBlock the potentially misaligned pointer
-/// @param b where the loaded bytes will be put
-/// @return a pair to indicate the aligned pointer to the base of the block
-/// and the misalignment, in bytes, of the source pointer
-/// \note The misalignment is in the range [ 0, sizeof(Block) [
-template<typename PtrT, typename Block>
-std::tuple<PtrT *, int>
-blockAlignedLoad(PtrT *pointerInsideBlock, Block *b) {
-    uintptr_t asUint = reinterpret_cast<uintptr_t>(pointerInsideBlock);
-    constexpr auto Alignment = alignof(Block), Size = sizeof(Block);
-    static_assert(Alignment == Size);
-    auto misalignment = asUint % Alignment;
-    auto *base = reinterpret_cast<PtrT *>(asUint - misalignment);
-    memcpy(b, base, Size);
-    return { base, misalignment };
-}
-
-}
