191614ce730fe69c7c95668629f5c31855c6a193
Jamie Pond <jamiepond259@gmail.com>
2025-06-08 16:53:23 -0700

    init

---
+++ inc/zoo/Any/eta/TEC.hpp
@@ -0,0 +1,48 @@
+
+#include "zoo/AlignedStorage.h"
+#include "zoo/pp/platform.h"
+#include "zoo/Any/Traits.h"
+#include "zoo/utility.h"
+
+#include "zoo/meta/copy_and_move_abilities.h"
+#include <type_traits>
+
+namespace zoo::any::eta {
+
+template<typename Policy_>
+struct TEC {
+    using Policy = Policy_;
+
+    TEC() = delete;
+    TEC(const TEC &) = delete;
+
+    using DefaultManager = typename Policy::MemoryLayout;
+
+    AlignedStorageFor<DefaultManager> space_;
+
+    using TokenType = void (TEC::*)();
+
+    constexpr static inline TokenType Token = nullptr;
+
+    template <typename Target, typename... Params>
+    TEC(TokenType, Params &&...params)
+        noexcept(
+            std::is_nothrow_constructible_v<Target, Params &&...>
+        )
+    {
+        space_.template build<Target>(std::forward<Params>(params)...);
+    }
+
+    ~TEC() {
+        space_.template as<DefaultManager>()->destroy();
+    }
+
+};
+
+struct Foo {
+    using MemoryLayout = void *;
+};
+
+static_assert(!std::is_move_constructible_v<TEC<Foo>>);
+
+}
